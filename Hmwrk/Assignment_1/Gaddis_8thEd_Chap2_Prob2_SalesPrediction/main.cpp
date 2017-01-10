15825b662d8e5f9c2347a8d4e90ab078a371b098
rcc <rcc>
2017-01-10 10:53:45 -0800

    Signed-off-by: rcc <rcc>

--- Hmwrk/Assignment_1/Gaddis_8thEd_Chap2_Prob2_SalesPrediction/main.cpp
+++ Hmwrk/Assignment_1/Gaddis_8thEd_Chap2_Prob2_SalesPrediction/main.cpp
@@ -1,12 +1,17 @@
 /* 
  * File:   main.cpp
- * Author: Dr. Mark E. Lehr
- * Created on July 19, 2016, 9:07 AM
- * Purpose:  Hello World Template
+ * Author: Gavin Mullins
+ * Created on January 10, 2017, 10:17 AM
+ * Purpose:  Sales Prediction
+The East Coast sales division of a company generates 58 percent of total sales. Based
+on that percentage, write a program that will predict how much the East Coast division
+will generate if the company has $8.6 million in sales this year.
  */
 
 //System Libraries Here
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
 //User Libraries Here
@@ -17,15 +22,20 @@ using namespace std;
 //Function Prototypes Here
 
 //Program Execution Begins Here
+
 int main(int argc, char** argv) {
     //Declare all Variables Here
+    int totSals,ecPercnt,ecSales;
     
     //Input or initialize values Here
-    
+    totSals=8.6,   //Total number of sales in millions of dollars
+    ecPercnt=.58;  //Percentage of sales that the East Coast division accounts for
+      
     //Process/Calculations Here
-    
+    ecSales=totSals*ecPercnt;
+       
     //Output Located Here
-    cout<<"Hello World"<<endl;
+    cout<<"The East Coast division of the company will generate $"<<4.988<<" million in sales."<<endl;
 
     //Exit
     return 0;
